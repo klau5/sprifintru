d83115c3bdf1893bb147f49da5c0b9c97c8e4dc5
klau5 <jografie@gmail.com>
2021-08-10 03:42:23 +0530

    smtp

--- api/contact.php
+++ api/contact.php
@@ -2,9 +2,9 @@
 
 require_once '../vendor/autoload.php';
 
-$transport = (new Swift_SmtpTransport('smtp.virgilio.it', 587))
-    ->setUsername('mattiacabrini@virgilio.it')
-    ->setPassword('Mattia88@');
+$transport = (new Swift_SmtpTransport('smtp.gmail.com', 587))
+    ->setUsername('kmvn22@gmail.com')
+    ->setPassword('ylC8hvAb4@l');
 
 $mailer = new Swift_Mailer($transport);
 
@@ -16,7 +16,7 @@ $message = htmlspecialchars($_POST['Message']);
 
 // create message
 $message = (new Swift_Message('Website Contact Form'))
-    ->setFrom(['mattiacabrini@virgilio.it' => 'Web'])
+    ->setFrom(['kmvn22@gmail.com' => 'Web'])
     ->setTo(['info@springfinancetrust.co'])
     ->setBody('<h3>Name: $name <br> Email: $email <br> Subject: $subject <br> Message: $message</h3>');
 
