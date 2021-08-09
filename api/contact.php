19a65a1aed158e9427934d45534a1ae0ee31cdb3
klau5 <45886598+klau5@users.noreply.github.com>
2021-08-10 00:18:36 +0530

    Update contact.php

--- api/contact.php
+++ api/contact.php
@@ -20,7 +20,7 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'poczta.o2.pl';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'dem.boyz.93@tlen.pl';                     //SMTP username
+    $mail->Username   = 'no-reply@10g.pl';                     //SMTP username
     $mail->Password   = 'belmondoe';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
