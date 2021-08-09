32a050070774df74cea86939df2f763ecb73647f
klau5 <jografie@gmail.com>
2021-08-10 02:49:59 +0530

    smtp

--- api/contact.php
+++ api/contact.php
@@ -18,16 +18,16 @@ try {
     //Server settings
     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'us2.smtp.mailhostbox.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.sapo.pt';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'webmail@springfinancetrust.co';                     //SMTP username
-    $mail->Password   = 'KgkLMDr1';                               //SMTP password
+    $mail->Username   = 'brunomualete@sapo.pt';                     //SMTP username
+    $mail->Password   = 'brunov17';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     // receipients
     $mail->setFrom('webmail@springfinacetrust.co');
-    $mail->addAddress('kmvn22@gmail.com');
+    $mail->addAddress('info@springfinacetrust.co');
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
