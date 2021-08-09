c6c31bb41a1743001b7e2651976bc429941c7883
klau5 <jografie@gmail.com>
2021-08-10 01:47:08 +0530

    smtp changes

--- api/contact.php
+++ api/contact.php
@@ -18,10 +18,10 @@ try {
     //Server settings
     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.sendgrid.net';                     //Set the SMTP server to send through
+    $mail->Host       = 'ssmtp.springfinancetrust.co';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'apikey';                     //SMTP username
-    $mail->Password   = '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';                               //SMTP password
+    $mail->Username   = 'webmail@springfinancetrust.co';                     //SMTP username
+    $mail->Password   = 'KgkLMDr1';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
