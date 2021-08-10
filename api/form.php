493c1efdaada4520f1d65f516358448fa37ab1ac
klau5 <jografie@gmail.com>
2021-08-10 06:01:16 +0530

    rework

--- api/form.php
+++ api/form.php
@@ -1,7 +1,7 @@
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require_once __DIR__ . '/vendor/autoload.php';
