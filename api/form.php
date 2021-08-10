a81284f8c20fe9789812e3bec0d528391dac6750
klau5 <jografie@gmail.com>
2021-08-10 06:09:27 +0530

    rework

--- api/form.php
+++ api/form.php
@@ -18,15 +18,15 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.springfinancetrust.co';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.umeagricfarm.co';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'webmail@springfinancetrust.co';                     //SMTP username
-    $mail->Password   = 'KgkLMDr1';                               //SMTP password
+    $mail->Username   = 'info@umeagricfarm.com';                     //SMTP username
+    $mail->Password   = 'DVrwQYF9';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     // receipients
-    $mail->setFrom('webmail@springfinancetrust.co');
+    $mail->setFrom('info@umeagricfarm.com');
     $mail->addAddress('info@springfinacetrust.co');
 
     //Content
